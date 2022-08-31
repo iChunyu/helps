b23e3d0bfb1e05b965a5e7bee3cbad52250da0cb
iChunyu <iChunyu@users.noreply.github.com>
2022-08-31 08:12:33 +0000

    deploy: 38fc70c3007176a3d341e0250917f8d94c138898

---
+++ sw.min.2b586b3ef4dad4a9069cc1f5081265e74a56d1d2d6ddbd7cccef2a4742060946.js
@@ -0,0 +1,1 @@
+navigator.serviceWorker&&navigator.serviceWorker.register("/helps/sw.js",{scope:"/"})
--- sw.min.f64fb8d201d1f8630596aa9dd14f82281dc1546c8ccca61a6bd182e5854cc09c.js
+++
@@ -1,1 +0,0 @@
-navigator.serviceWorker&&navigator.serviceWorker.register("/helps/sw.js",{scope:"/helps/"})
